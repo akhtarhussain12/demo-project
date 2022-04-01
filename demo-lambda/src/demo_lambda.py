7d44eebe90ca5608022c5b227f38cc1b33e27233
Hussain <akhtar.hussain@capgemini.com>
2022-04-01 11:20:54 +0530

    removed unnecessary import statments and code

--- demo-lambda/src/demo_lambda.py
+++ demo-lambda/src/demo_lambda.py
@@ -1,40 +1,30 @@
 import json
-# import boto3
 
 def main(event, context):
-    # sqs = boto3.client("sqs")
-    # dynamodb = boto3.resource('dynamodb')
-    message = {
-        "event_id" : "abc",
-        "event_name" : "newEvent",
-        "event_date" : "date"
-    }
+    try:
+        new_message = {
+            "event_id" : "abc",
+            "event_name" : "newEvent",
+            "event_date" : "date"
+        }
+        message = json.dumps(new_message)
 
-    print(json.dumps(message))
+    except Exception:
+        message = "Oops! something went wrong."
 
-    response = {
-        "status_code" : 200,
-        "message" : json.dumps(message)
-    }
-
-    return response
+    return message
 
 
-# import boto3
-# from uuid import uuid4
-# def main(event, context):
-#     try:
-#         sqs = boto3.client("sqs")
-#         dynamodb = boto3.resource('dynamodb')
-#         for record in event['Records']:
-#             message_name = record['sqs']['message']['name']
-#             object_key = record['sqs']['object']['key']
-#             size = record['sqs']['object'].get('size', -1)
-#             event_id = record['eventID']
-#             event_name = record['eventName']
-#             event_date = record['eventDate']
-#             dynamo_table = dynamodb.Table('demoTable')
-#             dynamo_table.put_item(Item={'Unique': str(uuid4()), 'Name': message_name, 'Object': object_key, 'Size': size, 'ID': event_id, 'Event': event_name, 'Date': event_date})
-#
-#     except:
-#         pass
+import boto3
+from uuid import uuid4
+def main(event, context):
+    sqs = boto3.client("sqs")
+    dynamodb = boto3.resource('dynamodb')
+    for record in event['Records']:
+        event_id = record['eventID']
+        event_name = record['eventName']
+        event_date = record['eventDate']
+        demo_table = dynamodb.Table('demoTable')
+        demo_table.put_item(Item={'Unique': str(uuid4()), 'ID': event_id, 'Event': event_name, 'Date': event_date})
+
+    return demo_table
